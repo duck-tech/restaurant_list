afdd47f209299ce4c52d0aa2c2470ed55180a0e3
duck-tech <babymis0628@gmail.com>
2021-10-24 21:37:54 +0800

    add category search

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 // params:id
 // querystring
 app.get('/search', (req, res) => {
-        const keyword = req.query.keyword
-        const restaurants = restaurantList.results.filter(restaurant => {
-            return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-        })
+        const keyword = req.query.keyword.toLowerCase().trim()
+        const restaurants = restaurantList.results.filter(
+            (restaurant) => restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+        )
         res.render('index', { restaurants: restaurants, keyword: req.query.keyword })
     })
     // start and listen on the Express server
