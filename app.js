c3995a1f12b17967b19ab4f1a762c338fec0ed2c
duck-tech <babymis0628@gmail.com>
2021-11-07 16:44:46 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const port = 3000
     // 連結mongoose
 const Restaurant = require("./models/Restaurant")
 const methodOverride = require("method-override")
+const routes = require('./routes')
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -33,14 +34,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-    // 瀏覽全部餐廳
-app.get("/", (req, res) => {
-    Restaurant.find({})
-        .lean()
-        .then(restaurantsData => res.render("index", { restaurantsData }))
-        .catch(err => console.log(err))
-})
-
+app.use(routes)
 
 // 搜尋特定餐廳
 app.get("/search", (req, res) => {
@@ -62,54 +56,6 @@ app.get("/search", (req, res) => {
             res.render("index", { restaurantsData: filterRestaurantsData, keywords })
         })
         .catch(err => console.log(err))
-})
-
-// 新增餐廳頁面
-app.get("/restaurants/new", (req, res) => {
-    res.render("new")
-})
-
-// 新增餐廳
-app.post("/restaurants", (req, res) => {
-    Restaurant.create(req.body)
-        .then(() => res.redirect("/"))
-        .catch(err => console.log(err))
-})
-
-
-// 瀏覽特定餐廳
-app.get("/restaurants/:restaurantId", (req, res) => {
-    const { restaurantId } = req.params
-    Restaurant.findById(restaurantId)
-        .lean()
-        .then(restaurantData => res.render("show", { restaurantData }))
-        .catch(err => console.log(err))
-})
-
-// 編輯餐廳頁面
-app.get("/restaurants/:restaurantId/edit", (req, res) => {
-    const { restaurantId } = req.params
-    Restaurant.findById(restaurantId)
-        .lean()
-        .then(restaurantData => res.render("edit", { restaurantData }))
-        .catch(err => console.log(err))
-})
-
-// 更新餐廳
-app.put("/restaurants/:restaurantId", (req, res) => {
-    const { restaurantId } = req.params
-    Restaurant.findByIdAndUpdate(restaurantId, req.body)
-        .then(() => res.redirect(`/restaurants/${restaurantId}`))
-        .catch(err => console.log(err))
-})
-
-// 刪除餐廳
-app.delete('/restaurants/:restaurantId', (req, res) => {
-    const id = req.params.restaurantId
-    return Restaurant.findById(id)
-        .then(restaurant => restaurant.remove())
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
 })
 
 // start and listen on the Express server
---
+++ routes/modules/restaurants.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = express.Router()
+const Restaurant = require("../../models/Restaurant")
+
+// 新增餐廳頁面
+router.get("/new", (req, res) => {
+    res.render("new")
+})
+
+// 新增餐廳
+router.post("/", (req, res) => {
+    Restaurant.create(req.body)
+        .then(() => res.redirect("/"))
+        .catch(err => console.log(err))
+})
+
+
+// 瀏覽特定餐廳
+router.get("/:restaurantId", (req, res) => {
+    const { restaurantId } = req.params
+    Restaurant.findById(restaurantId)
+        .lean()
+        .then(restaurantData => res.render("show", { restaurantData }))
+        .catch(err => console.log(err))
+})
+
+// 編輯餐廳頁面
+router.get("/:restaurantId/edit", (req, res) => {
+    const { restaurantId } = req.params
+    Restaurant.findById(restaurantId)
+        .lean()
+        .then(restaurantData => res.render("edit", { restaurantData }))
+        .catch(err => console.log(err))
+})
+
+// 更新餐廳
+router.put("/:restaurantId", (req, res) => {
+    const { restaurantId } = req.params
+    Restaurant.findByIdAndUpdate(restaurantId, req.body)
+        .then(() => res.redirect(`/restaurants/${restaurantId}`))
+        .catch(err => console.log(err))
+})
+
+// 刪除餐廳
+router.delete('/:restaurantId', (req, res) => {
+    const id = req.params.restaurantId
+    return Restaurant.findById(id)
+        .then(restaurant => restaurant.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
+
+module.exports = router
